dd37eb3e499c7701f9e1d6ef1231813b51838aac
Jovan Komlenac <komlenacjovan@yahoo.com>
2021-05-29 19:10:10 +0200

    Changed names to be polite

--- src/AplicationClientSide.java
+++ src/AplicationClientSide.java
@@ -1,6 +1,6 @@
 import userInterface.UI;
 
-public class PogavstinaAplicationClientSide {
+public class AplicationClientSide {
     public static void main(String[] args) {
         UI ui = new UI();
         ui.start();
